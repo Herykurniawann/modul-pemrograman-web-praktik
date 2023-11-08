dda8bd2c6700aaebd07d5e93ff5b11c1887b87ce
ryku <herykurniawansartono@gmail.com>
2023-11-08 19:24:22 +0700

    update

--- bab_4_integrasi_flask_dan_mysql/myapp.py
+++ bab_4_integrasi_flask_dan_mysql/myapp.py
@@ -1,7 +1,30 @@
 # import library third party
-from  flask import Flask, render_template
-from flask_mysql import MySQL
+from flask import Flask,render_template
+from flask_mysqldb import MySQL
 
-# inisiasi object Flask
+# init main app
 app = Flask(__name__)
 
+# database config
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB']='flask_praktikum'
+
+# init mysql
+mysql = MySQL(app)
+
+# set route default
+@app.route('/')
+# function name
+def home():
+#   cursor koneksi mysql
+    cur = mysql.connection.cursor()
+#   execute query
+    cur.execute("SELECT * FROM users")
+#   fetch hasil query masukkan ke variable data
+    data = cur.fetchall()
+#   close koneksi mysql
+    cur.close()
+
+#   render array data sebagai users
